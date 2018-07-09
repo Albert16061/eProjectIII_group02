98021463be75064c174941d8178e52853395dca9
Albert16061 <40551377+Albert16061@users.noreply.github.com>
2018-07-09 14:02:13 +0700

    Create_Index_branch

---
+++ Dr4rum_eProjectIII/Dr4rum_eProjectIII/Controllers/IndexController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Dr4rum_eProjectIII.Models;
+
+namespace Dr4rum_eProjectIII.Controllers
+{
+    public class IndexController : Controller
+    {
+        Dr4rumEntities db = new Dr4rumEntities();
+        // GET: Index
+        public async Task<ActionResult> Index()
+        {
+            var listGroup = db.Groups.Where(a => a.SetV == true).ToList();
+            var topicTitle = (from a in db.Groups
+                              from b in db.Categories
+                              from c in db.Topics
+                              where a.Group_Name == b.Group_Name && b.Category_Name == c.Category_Name
+                               && c.setV == true
+                              orderby c.Topic_Title descending
+                              select c.Topic_Title).First();
+            ViewBag.key = topicTitle;
+            return View(listGroup);
+        }
+    }
+}
