8e68ea9eb5ba850a3063d6d5c9d304cfa4627607
Albert16061 <40551377+Albert16061@users.noreply.github.com>
2018-07-10 17:35:46 +0700

    List of Category
    ver 1.0
    get info from index
    display info ok

--- Dr4rum_eProjectIII/Dr4rum_eProjectIII/Controllers/IndexController.cs
+++ Dr4rum_eProjectIII/Dr4rum_eProjectIII/Controllers/IndexController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +17,21 @@ namespace Dr4rum_eProjectIII.Controllers
         {
             var listGroup = db.Groups.Where(a => a.SetV == true).ToList();
             return View(listGroup);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> ListCategories(string Category_Name)
+        {
+            //var ListCat = (from c in db.Categories
+            //              from t in db.Topics
+            //              where c.Category_Name == t.Category_Name && t.Category_Name == catName &&t.setV == true
+            //              orderby t.date descending
+            //              select t).ToList();
+
+           // List<Topic> topic = db.Topics.Where(a => a.Category_Name == Category_Name && a.setV == true).ToList();
+
+            var listTopic = db.Categories.Where(a => a.Category_Name == Category_Name).ToList();
+            return View(listTopic);
         }
     }
 }
