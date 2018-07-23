8b7475f914397354a69a0951eee7efa381e1b0e3
Albert16061 <40551377+Albert16061@users.noreply.github.com>
2018-07-24 01:13:15 +0700

    create Thien-HUe branch

---
+++ Dr4rum_eProjectIII/Dr4rum_eProjectIII/Controllers/AccountsController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Dr4rum_eProjectIII.Models;
+
+namespace Dr4rum_eProjectIII.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private Dr4rumEntities3 db = new Dr4rumEntities3();
+        [HttpGet]
+        // GET: Accounts
+        public async Task<ActionResult> Index()
+        {
+            List<Account> ListAcc = db.Accounts.ToList();
+            return View(ListAcc);
+        }
+
+        // GET: Accounts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Accounts.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        // GET: Accounts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Accounts/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Acc_ID,UserName,Password,FirstName,LastName,Email,Address,Birthday,Phone,Gender,Role,Incognito,SetV,Speciality,Experience,Achievement,Avatar")] Account account, HttpPostedFileBase postedFile)
+        {
+            //kiem tra neu dl hop le
+            if (ModelState.IsValid)
+            {
+                //tao bien luu ten file hinh
+                var filename = Path.GetFileName(postedFile.FileName);
+                // luu duong dan file hinh
+                var path = Path.Combine(Server.MapPath("~/Image/Avatar"), filename);
+                if (System.IO.File.Exists(path))
+                {
+                    ViewBag.thongbao = "Avatar is Existed!";
+                }
+                else
+                {
+                    postedFile.SaveAs(path);
+                }
+                account.Avatar = postedFile.FileName;
+                db.Accounts.Add(account);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        // GET: Accounts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Accounts.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        // POST: Accounts/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Acc_ID,UserName,Password,FirstName,LastName,Email,Address,Birthday,Phone,Gender,Role,Incognito,SetV,Speciality,Experience,Achievement")] Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(account).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(account);
+        }
+
+        // GET: Accounts/delete/5
+        public ActionResult Delete(int? id)
+        {
+            var result = db.Accounts.Where(a => a.Acc_ID == id).SingleOrDefault();
+            if (result != null)
+            {
+                result.SetV = false;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult recovery(int? id)
+        {
+            var result = db.Accounts.Where(a => a.Acc_ID == id).SingleOrDefault();
+            if (result != null)
+            {
+                result.SetV = true;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Dr4rum_eProjectIII/Dr4rum_eProjectIII/Controllers/GroupsController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Dr4rum_eProjectIII.Models;
+
+namespace Dr4rum_eProjectIII.Controllers
+{
+    public class GroupsController : Controller
+    {
+        private Dr4rumEntities3 db = new Dr4rumEntities3();
+
+        // GET: Groups
+        public ActionResult Index()
+        {
+            return View(db.Groups.ToList());
+        }
+
+        // GET: Groups/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Groups/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Group_Name,SetV")] Group group)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Groups.Add(group);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(group);
+            }
+            catch (Exception)
+            {
+                ViewBag.err = "This Group name had been existed yet! Please choice an other name!";
+                return View();
+            }
+        }
+
+        // GET: Groups/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Group group = db.Groups.Find(id);
+            if (group == null)
+            {
+                return HttpNotFound();
+            }
+            return View(group);
+        }
+
+        // POST: Groups/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            var resultGroup = db.Groups.Where(a => a.Group_Name == id).SingleOrDefault();
+            if (resultGroup != null)
+            {
+                resultGroup.SetV = false;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult Recovery_Groups(string id)
+        {
+            var resultGroup = db.Groups.Where(a => a.Group_Name == id).SingleOrDefault();
+            if (resultGroup != null)
+            {
+                resultGroup.SetV = true;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
